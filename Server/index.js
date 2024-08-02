5d80435c5efcf0a96cd75f3dcdd05c1c870b98c5
Abhishek <abhishekb740@gmail.com>
2024-08-02 22:41:58 +0530

    fix: updated cors

--- Server/index.js
+++ Server/index.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors(
   {
     origin: [
-        "http://localhost:5173/",
-        "https://wallet-hunter-rosy.vercel.app/"
+        "http://localhost:5173",
+        "https://wallet-hunter-rosy.vercel.app"
     ],
     methods: ["POST", "GET", "HEAD", "PUT", "DELETE", "PATCH"],
     credentials: true,
